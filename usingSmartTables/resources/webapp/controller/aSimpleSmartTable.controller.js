c8d52a6baa8db522b4faf4dc493f55f0bda5bd5d
skye0402 <36907475+skye0402@users.noreply.github.com>
2020-08-26 23:07:53 +0900

    Revert "Part 2 completion."
    This reverts commit 20121618cab7c3a6d8b575ac18874b75ef6d808d.

--- usingSmartTables/resources/webapp/controller/aSimpleSmartTable.controller.js
+++ usingSmartTables/resources/webapp/controller/aSimpleSmartTable.controller.js
@@ -5,18 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("usingSmartTables.usingSmartTables.controller.aSimpleSmartTable", {
 		onInit: function () {
-			this._oView = this.getView();
-			this._oComponent = sap.ui.component(sap.ui.core.Component.getOwnerIdFor(this._oView));
-			this._oModel = this._oComponent.getModel("flightData");
-			this._oView.setModel(this._oModel);
-		},
-		onInitialise: function (oEvent) {
-			this.oTable = oEvent.getSource()._oTable;
-			this.oTable.setEnableColumnFreeze(true);
-		},
-		onBeforeExport: function (oEvt) {
-			var mExcelSettings = oEvt.getParameter("exportSettings");
-			mExcelSettings.worker = true;
+
 		}
 	});
 });
